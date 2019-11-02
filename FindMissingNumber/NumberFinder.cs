849ab54f0114463e256fba5bce6f371e4e131218
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-02 20:47:08 +0100

    Add project files.

---
+++ FindMissingNumber/NumberFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindMissingNumber
+{
+    class NumberFinder
+    {
+        public void FindNumber(int[] myArray)
+        {
+            int amountOfNumbers = myArray.Length;
+            var total = (amountOfNumbers + 1) * (amountOfNumbers + 2) / 2;
+            for (int i = 0; i < amountOfNumbers; i++)
+                total -= myArray[i];
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ FindMissingNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindMissingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.The array of numbers is unsorted. Only one number is missing");
+
+            int[] numberArray_1 = new int[9] { 1, 2, 3, 4, 6, 7, 8, 9, 10 };
+            int[] numberArray_2 = new int[9] { 7, 2, 3, 6, 5, 9, 1, 4, 8 };
+            int[] numberArray_3 = new int[9] { 10, 5, 1, 2, 4, 6, 8, 3, 9 };
+
+            var missingNumber = new NumberFinder();
+            missingNumber.FindNumber(numberArray_1);
+            missingNumber.FindNumber(numberArray_2);
+            missingNumber.FindNumber(numberArray_3);
+
+            Console.ReadKey();
+        }
+    }
+}
